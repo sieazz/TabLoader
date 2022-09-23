e159ae693c33ebff15c7bff9377bbebd30091e02
sieazz <sieazz@outlook.com>
2022-09-23 22:03:03 +0900

    Implement saving tabs on bookmark folder Other bookmarks > tabLoader > folder

--- popup/script.js
+++ popup/script.js
@@ -1,38 +1,57 @@
 let saveInput = document.querySelector("#save-tabs input");
 let saveButton = document.querySelector('#save-tabs button');
 
+let rootId;
+let supportedProtocols = ["https:", "http:", "ftp:", "file:"];
 
-// save tabs
+
+/* Setting bookmarks */
+// Get the id of root(tabLoader folder)
+// Create root if it not exists
+browser.bookmarks.search({title: "tabLoader"})
+    .then((res) => {
+        if (res.length === 0) {
+            browser.bookmarks.create({title: "tabLoader", type: "folder"})
+            .then((node) => {
+                rootId = node.id;
+            });
+        } else {
+            // It should be guarenteed that
+            // There is no other folder named "tabLoader"..
+            rootId = res[0].id;
+        }
+    });
+
+
+/* Save tabs */
 saveButton.addEventListener("click", (e) => {
     browser.tabs.query({ currentWindow: true })
         .then(res => {
-            let tabList = {};
-            tabList["name"] = saveInput.value;
-            tabList["tabs"] = [];
+            validTabList = [];
             
-            // Filter tabs without url, e.g., start page of browser
+            // Tab filtering
             res.forEach(tab => {
-                if (tab.url) {
-                    tabList["tabs"].push(
-                        {
-                            "title": tab.title,
-                            "url": tab.url,
-                            "favIconUrl": tab.favIconUrl
-                        }
-                    );
-                };
+                let url = document.createElement('a');
+                url.href = tab.url;
+                
+                if (supportedProtocols.includes(url.protocol)) {                    
+                    validTabList.push({title: tab.title, url: tab.url});
+                }
             });
 
             
-            if (tabList["tabs"].length > 0) {
-                
+            if (validTabList.length > 0) {
+                title = saveInput.value;
+
+                browser.bookmarks.create({title: title, type: "folder", parentId: rootId})
+                    .then(node => {
+                        validTabList.forEach(tab => {
+                            browser.bookmarks.create({title: tab.title, type:"bookmark", url: tab.url, parentId: node.id});
+                        });
+                    });
             }
-        }) 
-        .catch(err => {
-
-        });
-    
-})
+        })
+});
 
 
-// load tabs
+/* load tabs */
